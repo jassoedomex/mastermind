98f66dc7418266fad7b095022af9a7323b853b1b
fernandotorres24 <165342671+fernandotorres24@users.noreply.github.com>
2024-04-08 19:43:26 -0400

    creation of document

--- mastermind/actions.py
+++ mastermind/actions.py
@@ -1,2 +1,7 @@
 def change_pins(color,location, board):
     pass
+def select(color,location, board)
+    pass
+def move (location, color, board)
+    pass
+def correct(code, win, lose, board)
---
+++ mastermind/board.py
@@ -0,0 +1,13 @@
+def create_new_board()
+    pass
+def number_of_lines(quantity, color)
+    pass
+def number_of_holes(quantity, color)
+    pass
+def number_of_tries(color)
+    pass
+def code (win, lose)
+    pass
+def you_won (screen)
+    pass
+def you_lose(screen)
