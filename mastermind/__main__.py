dc0e03f8fe42bc099f082d901dae8063ad431829
nunga31 <165337581+nunga31@users.noreply.github.com>
2024-04-14 15:24:17 -0400

    Update __main__.py

--- mastermind/__main__.py
+++ mastermind/__main__.py
@@ -1,15 +1,3 @@
-from board import create_new_board
-from board import number_of_lines
-from board import number_of_holes
-from board import number_of_tries
-from board import code
-from board import you_won
-from board import you_lose
-from actions import change_pins
-from actions import select
-from actions import move
-from actions import correct
-
 name = input("tell me your name")
 print("Nice to meet you", name)
 with open('README.md', 'r') as f:
@@ -22,4 +10,14 @@ with open('README.md', 'r') as f:
       exit()
     else :
       print("I did not understand this command.")
-     
+from board import create_new_board
+from board import number_of_lines
+from board import number_of_holes
+from board import number_of_tries
+from board import code
+from board import you_won
+from board import you_lose
+from actions import change_pins
+from actions import select
+from actions import move
+from actions import correct
