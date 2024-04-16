8cae21ee1d5876d7b85aa91c3425256df7f2726b
nunga31 <165337581+nunga31@users.noreply.github.com>
2024-04-16 08:07:40 -0400

    Update __main__.py

--- mastermind/__main__.py
+++ mastermind/__main__.py
@@ -1,15 +1,3 @@
-name = input("tell me your name")
-print("Nice to meet you", name)
-with open('README.md', 'r') as f:
-  print(f.read())
-  while True:
-    command = input ("Enter a command:")
-    if command == "hello":
-      hello()
-    elif command == "q":
-      exit()
-    else :
-      print("I did not understand this command.")
 from board import create_new_board
 from board import number_of_lines
 from board import number_of_holes
@@ -21,3 +9,17 @@ from actions import change_pins
 from actions import select
 from actions import move
 from actions import correct
+from board import print_board
+
+name = input("tell me your name")
+print("Nice to meet you", name)
+with open('README.md', 'r') as f:
+  print(f.read())
+  while True:
+    command = input ("Enter a command:")
+    if command == "start":
+      create_new_board()
+    elif command == "q":
+      exit()
+    else :
+      print("I did not understand this command.")        
