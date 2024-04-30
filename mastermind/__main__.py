f460d730c7f9df9e07d1e1ffac404f330563355d
fernandotorres24 <165342671+fernandotorres24@users.noreply.github.com>
2024-04-30 09:35:58 -0400

    changes part 5

--- mastermind/__main__.py
+++ mastermind/__main__.py
@@ -1,11 +1,14 @@
 from board import create_new_board, number_of_lines, number_of_holes, number_of_tries, code, you_won, you_lose, print_board
 from actions import change_pins, select, move, correct
+from random import choices
 
 name = input("tell me your name")
 print("Nice to meet you", name)
 with open('README.md', 'r') as f:
   print(f.read())
 board, verification = create_new_board()
+colors = [r, b, g]
+code = choices(colors,k=3)
 current_row = 0
 while True:
     print_board(board, verification)
@@ -17,8 +20,6 @@ while True:
        location = input("Select the location of the pin")
        location = int(location)
        change_pins(color,[current_row, location],board)
-    elif command == "color":
-       select(None,None,board)
     elif command == "next":
        
        current_row = move(None,None,board)
--- mastermind/actions.py
+++ mastermind/actions.py
@@ -5,8 +5,16 @@ def change_pins(color,location, board):
 def select(color,location, board):
     print("set color")
 
-def move (location, color, board):
+def move (board):
     print("Next road")
 
-def correct(code, win, lose, board):
-    print("Result:")
+def correct(code, board):
+    verication_code = code
+    for code_color, board_color in zip(code, row):
+        if code_color == board_color:
+            colors = True
+            pint("you can mov eto the next row")
+        else:
+            colors = False
+            print ("you need to try something new")
+    return verication_code
